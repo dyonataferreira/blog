dfe1b08bce3c8405a5c2e6098a2c4ed1adfbe455
Dyonata Ferreira da Silva <dyonata@usp.br>
2025-12-05 18:09:06 -0300

    Versão 1: Views funcionais sem forms

---
+++ posts/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Post
+
+admin.site.register(Post)
+
+# dyonata
+# dyonata.py@gmail.com
+# 86131406
---
+++ posts/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
+from .models import Post
+
+# LISTAR
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'post_list.html', {'posts': posts})
+
+# DETALHE
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'post_detail.html', {'post': post})
+
+# CRIAR
+def post_create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        Post.objects.create(title=title, content=content)
+        return redirect('post_list')
+    return render(request, 'post_form.html')
+
+# EDITAR
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.title = request.POST.get('title')
+        post.content = request.POST.get('content')
+        post.save()
+        return redirect('post_detail', pk=post.pk)
+    return render(request, 'post_form.html', {'post': post})
+
+# DELETAR
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post_list')
+    return render(request, 'post_confirm_delete.html', {'post': post})
+
